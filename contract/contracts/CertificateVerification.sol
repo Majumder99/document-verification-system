048ce602b864a730d04cf0d4d556ddd4fab6489b
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-12-14 00:42:33 +0600

    done changes

--- contract/contracts/CertificateVerification.sol
+++ contract/contracts/CertificateVerification.sol
@@ -3,7 +3,9 @@ pragma solidity >=0.4.1;
 
 contract CertificateVerification {
 
-    mapping(string => bool) documentAddTimeMap; //contains when documents was added
+ event outputResult(bool);
+
+    mapping(string => uint256) documentAddTimeMap; //contains when documents was added
     mapping(string => address) documentAddKeyMap; //contains public key of the user
     mapping(address => bool) verifiedUser; //contains verified users 
     mapping(address => string) userFilesCid; //contains user's ipfs
@@ -19,30 +21,42 @@ contract CertificateVerification {
         _;
     }
 
+    //only student can verify files
+    modifier student_check(){
+        require(msg.sender != owner, "Sender have to be a student");
+        _;
+    }
+
     //only owner can add files to the contract
     // string memory ipfs_cid
     function add_files(string memory ipfs_cid) public owner_check returns(bool){
         //check if already added
         // return ipfs_cid;
         // return false;
-        // if (documentAddTimeMap[ipfs_cid] > 0) {
-        //     //already added by someone else
-        //     return documentAddTimeMap[ipfs_cid];
-        // }
-        // //add now
-        // uint256 timeAdded = block.timestamp;
-        // documentAddTimeMap[ipfs_cid] = timeAdded;
-        documentAddTimeMap[ipfs_cid] = true;
-        return documentAddTimeMap[ipfs_cid];
+        if (documentAddTimeMap[ipfs_cid] > 0) {
+            //already added by someone else
+            // return documentAddTimeMap[ipfs_cid];
+            emit outputResult(false);
+            return false;
+        }
+        //add now
+        uint256 timeAdded = block.timestamp;
+        documentAddTimeMap[ipfs_cid] = timeAdded;
+        emit outputResult(true);
+        return true;
+        // documentAddTimeMap[ipfs_cid] = true;
+        // return documentAddTimeMap[ipfs_cid];
     }
 
     //this will verify user's uploaded document
-    function verifyDocument(string memory ipfs_cid) public returns (bool){
-        if (documentAddTimeMap[ipfs_cid] == true) {
+    function verifyDocument(string memory ipfs_cid) public student_check returns(bool){
+        if (documentAddTimeMap[ipfs_cid] > 0) {
             verifiedUser[msg.sender] = true;
             userFilesCid[msg.sender] = ipfs_cid;
+            emit outputResult(true);
             return true;
         }
+        emit outputResult(false);
         return false;
     }
 
