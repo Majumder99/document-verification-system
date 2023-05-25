8e73ec6f3b482d9d9d5a24faf995d59c40017e22
Sourav Majumder <souravmajumderk99@gmail.com>
2023-05-26 04:22:23 +0600

    updated contract

--- contract/contracts/CertificateVerification.sol
+++ contract/contracts/CertificateVerification.sol
@@ -1,15 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.1;
+pragma solidity 0.8.19;
 
 contract CertificateVerification {
 
     event outputResult(bool);
     event outputCid(string);
+    event outputHash(bool);
 
     mapping(string => uint256) documentAddTimeMap; //contains when documents was added
     mapping(string => address) documentAddKeyMap; //contains public key of the user
     mapping(address => bool) verifiedUser; //contains verified users 
     mapping(address => string) userFilesCid; //contains user's ipfs
+    mapping(string => bool) hashValue;
     address payable public owner;
 
     constructor() {
@@ -18,43 +20,43 @@ contract CertificateVerification {
 
     //setting the user who deployed this contract as owner
     modifier owner_check(){
-        require(owner == msg.sender, "Sender is not owner");
+        require(owner == msg.sender, "User have to be admin");
         _;
     }
 
     //only student can verify files
     modifier student_check(){
-        require(msg.sender != owner, "Sender have to be a student");
+        require(msg.sender != owner, "User have to be a student");
         _;
     }
 
     //only owner can add files to the contract
     function add_files(string memory ipfs_cid) public owner_check returns(bool){
-        if (documentAddTimeMap[ipfs_cid] > 0) {
+        if (!hashValue[ipfs_cid]) {
+            //add now
+            hashValue[ipfs_cid] = true;
+            emit outputResult(true);
+            return true;
+        }else{
             emit outputResult(false);
             return false;
         }
-        //add now
-        uint256 timeAdded = block.timestamp;
-        documentAddTimeMap[ipfs_cid] = timeAdded;
-        emit outputResult(true);
-        return true;
     }
 
     //this will verify user's uploaded document
     function verifyDocument(string memory ipfs_cid) public student_check returns(bool){
-        if (documentAddTimeMap[ipfs_cid] > 0) {
-            verifiedUser[msg.sender] = true;
-            emit outputResult(true);
-            return true;
-        }
+       if(hashValue[ipfs_cid]){
+        emit outputResult(true);
+        return true;
+       }else{
         emit outputResult(false);
         return false;
+       }
     }
 
     //this is for verify and apply button
     function verifyApplyDocument(string memory ipfs_cid) public student_check returns(bool){
-        if (documentAddTimeMap[ipfs_cid] > 0) {
+        if(hashValue[ipfs_cid]){
             verifiedUser[msg.sender] = true;
             userFilesCid[msg.sender] = ipfs_cid;
             emit outputResult(true);
